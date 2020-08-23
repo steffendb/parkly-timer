c27c954611d4fea42e2ed478d706d6a8ec55513c
Steffendb <2060550+steffendb@users.noreply.github.com>
2020-08-24 00:39:41 +0200

    Update js.js

--- js.js
+++ js.js
@@ -1,13 +1,7 @@
 const targetPanel = document.getElementById("vouchers");
 
-targetPanel.classList.toggle("open");
-
-document.addEventListener(
-  "click",
-  function (event) {
-    if (event.target.matches(".toggle")) {
-      targetPanel.classList.toggle("open");
-    }
-  },
-  false
-);
+document.addEventListener('click', function (event) {
+  if (event.target.matches('.toggle')) {
+    targetPanel.classList.toggle("open");
+  }
+}, false);
